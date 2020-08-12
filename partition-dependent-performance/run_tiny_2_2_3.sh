0296043318436e55b113e2b6da807cd9a6cededf
Jonathan Pietarila Graham <jpietarilagraham@gmail.com>
2020-08-12 11:03:15 -0700

    sapling partition dependent performance experiments

---
+++ partition-dependent-performance/run_tiny_2_2_3.sh
@@ -0,0 +1,13 @@
+source /home/jgraham//github/spack/share/spack/setup-env.sh
+spack load mpich
+spack load boost
+spack load lua
+export VERSION=mpich_3.3-gcc_8.3.0
+export LD_LIBRARY_PATH=/home/jgraham/opt/${VERSION}/lib:/home/jgraham/opt/${VERSION}/lib64:${LD_LIBRARY_PATH}
+printenv LD_LIBRARY_PATH
+who
+
+mpiexec -n 12 /home/jgraham/github/flecsale/build-${VERSION}/apps/hydro/3d/hydro_3d -m /home/jgraham/github/instructions/meshes/26x78x130on12_2_2_3/26x78x130.g.012 -- -ll:cpu 2 -ll:gsize 0 –l:show_rsrv –ll:csize 3500 |& tee tiny_2_2_3.out
+
+who
+
